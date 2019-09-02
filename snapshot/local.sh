d7aca0c59d1ab87b801a08b20f55b2bc108ae452
bigrocs <bigrocs@qq.com>
2019-09-02 08:38:42 +0800

    修改事件

--- snapshot/local.sh
+++ snapshot/local.sh
@@ -1,7 +1,7 @@
 ##/etc/rc.local.d/local.sh
 #!/bin/sh
 /bin/echo "30   1   *   *   *   sh /vmfs/volumes/datastore1/crontabs/snapshot_creat.sh" >>/var/spool/cron/crontabs/root 
-/bin/echo "30   2   *   *   *   sh /vmfs/volumes/datastore1/crontabs/snapshot_del.sh" >>/var/spool/cron/crontabs/root
+/bin/echo "50   1   *   *   *   sh /vmfs/volumes/datastore1/crontabs/snapshot_del.sh" >>/var/spool/cron/crontabs/root
 kill $(cat /var/run/crond.pid)
 crond
 exit 0
--- snapshot/snapshot_creat.sh
+++ snapshot/snapshot_creat.sh
@@ -4,13 +4,13 @@ for i in `vim-cmd vmsvc/getallvms |  grep -v nosnap | awk '{print $1}' | grep -e
 do
 	vname=$(vim-cmd vmsvc/get.summary $i | grep name | awk '{ print $3 }' | cut -d \" -f 2)
     vannotation=$(vim-cmd vmsvc/get.summary $i | grep annotation | awk '{ print $3 }' | cut -d \" -f 2)
-    if [ $vannotation == 'auto_snapshot' || $vannotation == 'auto' ];then
+    if [ $vannotation = 'auto_snapshot' ];then
         vim-cmd vmsvc/snapshot.create $i $(date "+%F")
-        sleep 60s
+        sleep 30s
         if [ $? -eq 0 ];then
-            echo "$(date "+%F %T") : $vname snapshot create success.." >> ./schedule_snap.log
+            echo "$(date "+%F %T") : $vname snapshot create success.." >> ./vmfs/volumes/datastore1/crontabs/log/schedule_snap.log
         else
-            echo "$(date "+%F %T") : $vname snapshot create FAILED.." >> ./schedule_snap.log
+            echo "$(date "+%F %T") : $vname snapshot create FAILED.." >> ./vmfs/volumes/datastore1/crontabs/log/schedule_snap.log
         fi
     fi
 done
